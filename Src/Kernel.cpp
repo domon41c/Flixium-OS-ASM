c47097dba3f635ec17ac936f5dc22ec3d311c091
domon41c <98189741+domon41c@users.noreply.github.com>
2023-07-12 17:00:00 +0300

    Delete Src directory

--- Src/Kernel.cpp
+++
@@ -1,8 +0,0 @@
-#include "Text.cpp"
-
-extern "C" void _start() {
-    SetCursorPosition(PositionFromCoords(0, 0));
-    PrintString("hello world\n\rBebraaaaaa\n\r");
-    PrintString(HexToString(0x1234abcd));
-    return;
-}
